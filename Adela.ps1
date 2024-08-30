1ec9222fe28f45fb084319edcda96dc2c87178ed
priytamsh <33485156+priytamsh@users.noreply.github.com>
2024-08-30 21:52:07 +0530

    Create Adela.ps1

---
+++ Adela.ps1
@@ -0,0 +1,21 @@
+3.	configuration Adela
+4.	{
+5.	    Node IsWebServer
+6.	    {
+7.	        WindowsFeature IIS
+8.	        {
+9.	            Ensure               = 'Present'
+10.	            Name                 = 'Web-Server'
+11.	            IncludeAllSubFeature = $true
+12.	        }
+13.	    }
+14.	
+15.	    Node NotWebServer
+16.	    {
+17.	        WindowsFeature IIS
+18.	        {
+19.	            Ensure               = 'Absent'
+20.	            Name                 = 'Web-Server'
+21.	        }
+22.	    }
+23.	}
